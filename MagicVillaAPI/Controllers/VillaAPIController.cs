7e53a6651f5429f44a6cfd2b919f9d6277510f64
Hüseyin Baser <huseyin@wissenakademie.com>
2023-03-27 21:53:22 +0300

    Get Villa added

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVillaAPI.Models;
+﻿using MagicVillaAPI.Data;
+using MagicVillaAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVillaAPI.Controllers
@@ -8,12 +9,15 @@ namespace MagicVillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa> {
-            new Villa{Id=1,Name="Pool View" },
-            new Villa{Id=2,Name="Beach View" }
-            };
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(p=>p.Id==id);
+
         }
     }
     
--- MagicVillaAPI/Data/VillaStore.cs
+++ MagicVillaAPI/Data/VillaStore.cs
@@ -1,6 +1,12 @@
-﻿namespace MagicVillaAPI.Data
+﻿using MagicVillaAPI.Models;
+
+namespace MagicVillaAPI.Data
 {
-    public class VillaStore
+    public static class VillaStore
     {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+            new VillaDTO{Id=1,Name="Pool View" },
+            new VillaDTO{Id=2,Name="Beach View" }
+        };
     }
 }
